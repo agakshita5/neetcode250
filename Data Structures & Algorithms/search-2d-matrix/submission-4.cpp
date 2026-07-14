6b0c58de04e6931abecb25f8f7bac8dab8ad1a29
Akshita Agarwal <akshita.ag2020@gmail.com>
2026-07-14 12:03:43 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& v, int t) {
+        // binary search on rows
+        // binary search on selected row
+        // one-binary solution => 1D -> 2D
+
+        int m = v.size(), n = v[0].size();
+        int l=0, h=(m*n)-1;
+
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(v[mid/n][mid%n] > t) h=mid-1;
+            else if(v[mid/n][mid%n] < t) l=mid+1;
+            else return true;
+        }
+        return false;
+    }
+};
