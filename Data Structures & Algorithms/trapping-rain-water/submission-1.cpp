a490fddc0edfa76cff9b367d433f3bb31dc96051
Akshita Agarwal <akshita.ag2020@gmail.com>
2026-08-07 20:36:04 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        //  maxleft maxright
+        
+        int n= h.size();
+
+        // L-R
+        vector<int> ml(n);
+        ml[0] = h[0];
+        for(int i=1;i<n;i++){
+            ml[i] = max(ml[i-1], h[i]);
+        }
+        // R-L
+        vector<int> mr(n);
+        mr[n-1] = h[n-1];
+        for(int i=n-2;i>=0;i--){
+            mr[i] = max(mr[i+1], h[i]);
+        }
+
+        // find sum of water trapped
+        int res=0;
+        for(int i=0;i<n;i++){
+            res+= min(ml[i], mr[i]) - h[i];
+        }
+        return res;
+        
+    }
+};
