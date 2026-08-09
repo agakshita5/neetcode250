9bf7d6f6b3c4d181194fb7903adae6d84ea4940d
Akshita Agarwal <akshita.ag2020@gmail.com>
2026-08-09 23:08:45 +0530

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        /*
+        // first max in first wnd
+        // sliding wnd and gwt max at each wnd
+        // O(n*k) time as 'k' size window all over arr
+        // and iterating though each elem within that wnd
+        int n = arr.size();
+        int i = 0;
+        vector<int> v;
+        while(i <= n-k){
+            int maxElem = arr[i];
+            for(int j=i+1;j<i+k;j++){
+                maxElem = max(maxElem, arr[j]);
+            }
+            v.push_back(maxElem);
+            i++;
+        }
+        return v;
+        */
+
+        // data struct - tells curr max elem of the wnd in O(1) time ––> heap
+        
+        // dequeue implementation
+        deque<int> dq;
+        int n= arr.size();
+        vector<int> v;
+
+        int i = 0;
+        while(i < n){
+            // while max elem (front()) is out of wnd – pop it
+            while(!dq.empty() && dq.front() <= i-k) dq.pop_front(); 
+            // simply check curr elem > back – pop from back
+            while(!dq.empty() && arr[dq.back()] < arr[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            
+            if(i >= k-1) v.push_back(arr[dq.front()]); // 1(2) 
+            i++;
+
+        }
+        return v;
+    }
+};
