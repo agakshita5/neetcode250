884780d1b4172297f517695ec8fe6c5af9dd0457
Akshita Agarwal <akshita.ag2020@gmail.com>
2026-09-18 05:11:55 +0530

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    set<pair<int,int>> visit;
+
+    int dfs(vector<vector<int>>& grid, pair<int,int> v){
+        // base case
+        int contri = 0, row = v.first, col = v.second;
+        if(row < 0 || col < 0 || row >= grid.size() || col >= grid[row].size() || grid[row][col] == 0) return 1;
+
+        // mark that cell with land as visited
+        visit.insert({row,col});
+        
+        // go left
+        if(!visit.contains({row, col-1})) contri += dfs(grid, {row,col-1});
+        // go right
+        if(!visit.contains({row, col+1})) contri += dfs(grid, {row,col+1});
+        // go up
+        if(!visit.contains({row-1, col})) contri += dfs(grid, {row-1,col});
+        // go down
+        if(!visit.contains({row+1, col})) contri += dfs(grid, {row+1,col});
+
+        return contri;
+    }
+
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int ans = 0;
+        for(int i=0; i < grid.size(); i++){
+            for(int j=0; j < grid[i].size(); j++){
+                if(grid[i][j] == 1){
+                    visit.insert({i,j});
+                    ans = dfs(grid, {i,j});
+                    return ans;
+                }
+            }
+        }
+    }
+};
